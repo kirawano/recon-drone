36fd8328d52d440586d56ca0542e6e7acaa8886e
theSaneHatter <139697271+theSaneHatter@users.noreply.github.com>
2026-03-23 12:32:42 -0700

    epsiten 67 :# robloc

---
+++ omh-capture.fish
@@ -0,0 +1,8 @@
+gst-launch-1.0 -v \
+    udpsrc port=5000 \
+    caps="application/x-rtp, media=video, encoding-name=H264, payload=96" ! \
+    rtph264depay ! \
+    h264parse ! \
+    avdec_h264 ! \
+    videoconvert ! \
+    autovideosink sync=false
