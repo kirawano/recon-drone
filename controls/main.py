b13c7fcf40b65a2c7b4b47ea7ad2ae99306e0942
Cole Thompson <coledt54@gmail.com>
2026-04-09 11:02:32 -0700

    james mogs leonarda

---
+++ controls/main.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from flask import *
+from time import sleep
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def root():
+    url_for('static', filename='controls.js')
+    print("hi")
+    return render_template('index.html')
+
+@app.route('/controller_data', methods=['POST'])
+def controller():
+    d = request.get_json()
+    print(d)
+    return render_template('index.html')
