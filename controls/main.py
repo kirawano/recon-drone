920b552fdaeb2f5f436174b969105094cbfed29e
kirawano <kirawano@waifu.club>
2026-04-27 12:37:21 -0700

    CHANGES

--- controls/main.py
+++ controls/main.py
@@ -1,4 +1,42 @@
 #!/usr/bin/env python3
+from gpiozero import *
+from time import sleep
+
+LMOTORPIN = 27
+LREVERSEPIN = 22
+
+RMOTORPIN = 23
+RREVERSEPIN = 24
+
+lm = Motor(forward=LMOTORPIN, backward=LREVERSEPIN, pwm=True)
+rm = Motor(forward=RMOTORPIN, backward=RREVERSEPIN, pwm=True)
+
+motors = [lm, rm]
+
+def drive_motor(motor, speed):
+	s = abs(speed)
+	if speed > 0:
+		motor.forward(speed=abs(s))
+	else:
+		motor.backward(speed=abs(s))
+
+# argument order: list(lm, rm), list(lstick, rstick)
+def arcade(motors, dir):
+	lraw = dir[0] - dir[1]
+	rraw = dir[0] + dir[1]
+	
+	# hack
+	if lraw > 1:
+		lraw = 1
+	elif lraw < -1:
+		lraw = -1
+	if rraw > 1:
+		rraw = 1
+	elif rraw < -1:
+		rraw = -1
+
+	drive_motor(motors[0], lraw)
+	drive_motor(motors[1], rraw)
 
 from flask import *
 from time import sleep
@@ -17,23 +55,10 @@ def root():
 @app.route('/controller_data', methods=['POST'])
 def controller():
     d = request.get_json()['ctrl']
-    print(d['ctrl'])
+    #drive_motor(rm, d[0])
+    arcade(motors, d)
     return jsonify({"success":"true"})
 
 def get_dirs():
 	return d
 
-from gpiozero import *
-from time import sleep
-
-MOTORPIN = 27
-REVERSEPIN = 22
-
-lm = Motor(forward=MOTORPIN, backward=REVERSEPIN, pwm=True)
-
-def drive_motor(motor, speed):
-	s = abs(speed)
-	if speed > 0:
-		motor.forward(speed=s)
-	else:
-		motor.backward(speed=s)
---
+++ start.fish
@@ -0,0 +1,1 @@
+fish omh-stream.fish & flask --app controls/main.py run --host=0.0.0.0
