5e4d20e7c973e3ba9bea816680bd8b523cc2ba1a
kirawano <kirawano@waifu.club>
2026-05-03 19:05:43 -0700

    hi

---
+++ controls/sock.py
@@ -0,0 +1,20 @@
+import socket
+import json
+from motor import arcade
+
+UDP_IP = "0.0.0.0"
+UDP_PORT = 5000
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+while True:
+	data, addr = sock.recvfrom(1024)
+	try:
+		message = json.loads(data.decode())
+		d = [message.get("l", 0), message.get("r", 0)]
+		arcade(d)
+	except json.JSONDecodeError:
+		print("received malformed packet")
+
+sock.close()
--- start.fish
+++ start.fish
@@ -1,1 +1,1 @@
-fish omh-stream.fish & flask --app controls/main.py run --host=0.0.0.0
+fish omh-stream.fish & python controls/sock.py
